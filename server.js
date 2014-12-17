2f64d737ec0cce54770a696a878a97d4a1227e4f
Clément Uster <clement.uster@gmail.com>
2014-12-17 07:26:08 +0100

    define the server api to manage notes

---
+++ server.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+//
+//conf
+//
+
+mongoose.connect('mongodb://localhost/my-notes');
+app.use(express.static(__dirname + 'public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(methodOverride());
+
+//
+//model 
+//
+
+var Note = mongoose.model('Note', {
+  title : String, 
+  content : String
+});
+
+//
+//routes
+//
+
+//listing notes
+app.get('/api/notes', function(req, res){
+  Note.find(function(err, notes){
+    if(err)
+      res.send(err);
+    res.json(notes);
+  });
+});
+
+//creating a note
+app.post('/api/notes', function(req, res){
+  Note.create({
+  	title : req.body.title,
+  	content : req.body.content
+  }, function(err, notes){
+      if(err)
+        req.send(err);
+      res.json(notes);
+  })
+});
+
+//deleting a note
+app.delete('/api/notes/:id', function(req, res){
+  Note.remove({
+  	_id : req.body.id
+  }, function(err, notes){
+  	  if(err)
+  	  	res.send(err);
+  	  res.json(notes);
+  })
+});
+
+//
+//the app!
+//
+app.listen(8080);
+console.log("App started on port 8080");
